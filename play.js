1f4ad496f1b113883bd3f84cc348a72a1913d6cf
TJ <teejaynam@gmail.com>
2023-06-15 23:10:30 -0400

    remove connect function, move to client to make more modular

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const net = require("net");
+const connect = require("./client")
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541,// PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf-8");
-
-  conn.on('data', () => {
-    console.log("you ded cuz you idled");
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
