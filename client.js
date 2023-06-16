80e4a30b3b5bb196e4296b6c3a5a64bd8ffb1806
TJ <teejaynam@gmail.com>
2023-06-16 00:06:06 -0400

    Add connect event to show that server connection establish, also to write name to server

--- client.js
+++ client.js
@@ -9,6 +9,14 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf-8");
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: TJN");
+  })
+
   conn.on('data', () => {
     console.log("you ded cuz you idled");
   });
