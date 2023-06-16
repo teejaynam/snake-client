8657a800b7ba7b2dd3e39b4d94c4d83a473acea6
TJ <teejaynam@gmail.com>
2023-06-15 23:04:56 -0400

    update to add event handler when disconnect to send a message

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541,// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf-8");
+
+  conn.on('data', () => {
+    console.log("you ded cuz you idled");
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
