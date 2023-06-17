b7e7888c6614f584ac8dcd102589497d2091e207
TJ <teejaynam@gmail.com>
2023-06-17 16:57:20 -0400

    update play to require input.js for input handling

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
 const net = require("net");
 const {connect} = require("./client");
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    console.log("Exiting program ...");
-    process.exit();
-  }
-};
-
-// interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const {setupInput, handleUserInput} = require("./input");
 
 console.log("Connecting ...");
 connect();
