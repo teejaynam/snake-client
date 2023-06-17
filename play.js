0b75ef1cb0d57d03c1af7c61cb2e5a12b334179a
TJ <teejaynam@gmail.com>
2023-06-17 16:49:30 -0400

    add setupInput function to create listener for keyboard input with stdin, and handeUserInput function to check for CTRL+C to exit program

--- play.js
+++ play.js
@@ -1,7 +1,26 @@
 const net = require("net");
 const {connect} = require("./client");
 
-// establishes a connection with the game server
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log("Exiting program ...");
+    process.exit();
+  }
+};
+
+// interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
