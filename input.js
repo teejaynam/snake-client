f4492f84855a4574492c7760e0631d89e2825b1a
TJ <teejaynam@gmail.com>
2023-06-18 14:12:15 -0400

    update input to use new constants module, add a clearInterval method to register when new movement keys are pressed and add seperate logic for non-interval key inputs

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
-const {keyActions} = require('./constants')
+const {keyActions,keyMessageActions} = require('./constants')
 // stores the active TCP connection object
 let connection;
+let currentKey; //for setting the interval for when movement key is pressed
 
 const handleUserInput = function(key) {
   if (key === '\u0003') {
@@ -8,8 +9,15 @@ const handleUserInput = function(key) {
     process.exit();
   }
 
+  if (key in keyMessageActions) {
+    keyMessageActions[key](connection);
+  } 
+
   if (key in keyActions) {
-    keyActions[key](connection);
+    if(currentKey) {
+      clearInterval(currentKey); //clears the setInterval from when you press a movement key
+    }
+    currentKey = keyActions[key](connection); //sets the movement + currentInterval
   }
 };
 
