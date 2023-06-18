bff7e24d5010644b37217a3f797b322f8cca5a93
TJ <teejaynam@gmail.com>
2023-06-18 14:11:29 -0400

    update keyAction movements to setInterval the movements just like the real snake game, also seperate non-interval key actions like special messages to a new object

--- constants.js
+++ constants.js
@@ -3,17 +3,21 @@ const PORT = 50541;
 const NAME = process.argv[2];
 
 const keyActions = {
-  w: (connection) => connection.write("Move: up"),
-  a: (connection) => connection.write("Move: left"),
-  s: (connection) => connection.write("Move: down"),
-  d: (connection) => connection.write("Move: right"),
+  w: (connection) => setInterval(() => connection.write("Move: up"), 50),
+  a: (connection) => setInterval(() => connection.write("Move: left"), 50),
+  s: (connection) => setInterval(() => connection.write("Move: down"), 50),
+  d: (connection) => setInterval(() => connection.write("Move: right"), 50),
+};
+
+const keyMessageActions = {
   b: (connection) => connection.write("Say: BANANA!!"),
   n: (connection) => connection.write("Say: NUTELLA!!"),
-};
+}
 
 module.exports = {
   IP,
   PORT,
   NAME,
-  keyActions
+  keyActions,
+  keyMessageActions
 }
